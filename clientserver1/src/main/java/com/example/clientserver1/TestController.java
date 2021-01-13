f8a8ebaccc3b284c8a053339f9035080434dbd19
yys <yuyingsheng@ningzefintech.com>
2021-01-13 17:37:51 +0800

    初始化

---
+++ clientserver1/src/main/java/com/example/clientserver1/TestController.java
@@ -0,0 +1,18 @@
+package com.example.clientserver1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Value("${spring.application.name}")
+    private String name;
+    @GetMapping("/getName")
+    public String tes1(){
+        return name;
+    }
+}
